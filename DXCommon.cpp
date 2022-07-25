bc54656f327bf5b39e410391f1325746e757eccb
DESKTOP-70283EI\mommo <k020g1236@g.neec.ac.jp>
2022-07-25 15:19:41 +0900

    CG4の作業ちょっと

--- DXCommon.cpp
+++ DXCommon.cpp
@@ -27,10 +27,11 @@ void DXCommon::InitializeDevice()
 	HRESULT result;
 #ifdef _DEBUG
 	//デバッグレイヤーをオンに
-	ComPtr<ID3D12Debug> debugController;
+	ComPtr<ID3D12Debug1> debugController;
 	if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
 	{
 		debugController->EnableDebugLayer();
+        debugController->SetEnableGPUBasedValidation(TRUE);
 	}
 #endif
 
